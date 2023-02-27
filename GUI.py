213781b3cfef5f139d3f07cf6b93ae5b97a924d0
My Name <myEmail@example.com>
2023-02-27 23:29:07 +0500

    Added displaying of game, and some design (that is cool!)

--- GUI.py
+++ GUI.py
@@ -22,24 +22,54 @@ class GraphicsField(GameField):
         self.left = left
         self.top = top
         self.cell_size = cell_size
-    
+
     def get_score(self):
         return self.field.get_score()
 
     def load_field(self, new_field):
         self.field = new_field
-    
+
     def get_field(self):
         return self.field
 
+    def set_text(self, screen, value, cords):
+        font_color = pygame.Color('#696153')
+        font = pygame.font.Font(None, 50)
+        text = font.render(str(value), True, font_color)
+        width_text = text.get_width() // 2
+        height_text = text.get_height() // 2
+        cords = cords[0] - width_text, cords[1] - height_text
+        screen.blit(text, cords)
+
     def render(self, screen):
+        # For each color there are unique color
+        color_values = {
+            2: '#eae0d4',
+            4: '#e9dfc3',
+            8: '#eead77',
+            16: '#ea9260',
+            32: '#f2765b',
+            64: '#f25836',
+            128: '#e6cc6c',
+            256: '#f1d14a',
+            512: '#efc451',
+            1024: '#ecc43f',
+            2048: '#eec22e',
+        }
         for i in range(self.width):
             for j in range(self.height):
-                pygame.draw.rect(screen, '#bbada0', (
-                    (self.left + i * self.cell_size, self.top + j * self.cell_size), (self.cell_size, self.cell_size)),
-                                 2)
-                #print(self.field.field[i][j])  # EXAMPLE: this returns the cell value
-                                                # None should be 0
+                cell_value = self.field.field[j][i]
+                cords = [self.left + i * self.cell_size, self.top + j * self.cell_size]
+                w, h = (self.cell_size, self.cell_size)
+                if cell_value:
+                    cell_color = color_values[cell_value]
+                else:
+                    cell_color = '#cdc1b5'
+                pygame.draw.rect(screen, cell_color, (cords, (w, h)))
+                pygame.draw.rect(screen, '#bbada0', (cords, (w + 4, h + 4)), 4)
+                if cell_value:
+                    x1, y1 = cords[0] + self.cell_size // 2, cords[1] + self.cell_size // 2
+                    self.set_text(screen, cell_value, (x1, y1))
 
 
 pygame.init()
@@ -62,7 +92,6 @@ while running:
         # TODO: winning message
         new_field = GameField()
         board.load_field(new_field)
-    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -79,7 +108,7 @@ while running:
             if key == pygame.K_RIGHT or key == pygame.K_d:
                 direction = Directions.RIGHT
             if direction is None:
-                pass # TODO: you cannot move like that message
+                pass  # TODO: you cannot move like that message
             else:
                 is_changed = board.get_field().make_move(direction)
                 if not is_changed:
